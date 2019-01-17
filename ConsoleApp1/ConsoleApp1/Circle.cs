5fff79a57573f98a576b0a44eaa36ff562042708
soginga <ssoginga@retrorabbit.co.za>
2019-01-17 13:12:10 +0200

    Added Circle implementation

---
+++ ConsoleApp1/ConsoleApp1/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Circle
+    {
+        void Area()
+        {
+            Console.Write("Enter side length: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Area is : " + (Math.PI * r * r) );
+            Console.ReadKey();
+        }
+    }
+}
