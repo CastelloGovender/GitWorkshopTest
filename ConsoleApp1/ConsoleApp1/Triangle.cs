911a78c2166f4de5bc123e6f1b24dd3b79d02dc0
DinolanNaidoo <dnaidoo@retrorabbit.co.za>
2019-01-17 13:04:45 +0200

    Calculate Area of Triangle

---
+++ ConsoleApp1/ConsoleApp1/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Triangle
+    {
+        double h;
+        double b;
+
+        public Triangle()
+        {
+            this.h = 0;
+            this.b = 0;
+        }
+
+        public Triangle(double height, double breadth)
+        {
+            this.h = height;
+            this.b = breadth;
+        }
+
+        public double Area_of_Triangle(double height, double breadth)
+        {
+            double area = 0.5 * height * breadth;
+            return area;
+        }
+    }
+}
